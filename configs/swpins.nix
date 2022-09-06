bf988d7333a5416d39915446780d724e71a54843
Jakub Skokan <jakub.skokan@havefun.cz>
2022-09-06 10:42:24 +0200

    swpins: disable core auto-updates

--- configs/swpins.nix
+++ configs/swpins.nix
@@ -18,6 +18,17 @@ let
     };
   };
 in {
+  confctl.swpins.core.pins = {
+    nixpkgs = {
+      type = "git-rev";
+      git-rev = {
+        url = "https://github.com/NixOS/nixpkgs";
+        update.ref = "refs/heads/nixos-unstable";
+        update.auto = false;
+      };
+    };
+  };
+
   confctl.swpins.channels = {
     nixos-stable = { nixpkgs = nixpkgsBranch "nixos-22.05"; };
 

54a5a1ec2bb8ad4711e51404a93a79c5e5c47efd
Jakub Skokan <jakub.skokan@havefun.cz>
2023-06-06 12:03:42 +0200

    swpins: update nixos-stable to 23.05

--- configs/swpins.nix
+++ configs/swpins.nix
@@ -30,7 +30,7 @@ in {
   };
 
   confctl.swpins.channels = {
-    nixos-stable = { nixpkgs = nixpkgsBranch "nixos-22.11"; };
+    nixos-stable = { nixpkgs = nixpkgsBranch "nixos-23.05"; };
 
     os-staging = { vpsadminos = vpsadminosBranch "staging"; };
 

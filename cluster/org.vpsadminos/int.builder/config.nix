69c42e41b7f2278c4bb413c7d6400b0271e26c5d
Jakub Skokan <jakub.skokan@havefun.cz>
2025-05-28 10:07:50 +0200

    builder: use cache.vpsadminos.org

--- cluster/org.vpsadminos/int.builder/config.nix
+++ cluster/org.vpsadminos/int.builder/config.nix
@@ -13,5 +13,10 @@
     "/nix/var/cache/ccache"
   ];
 
+  nix.settings = {
+    substituters = [ "https://cache.vpsadminos.org" ];
+    trusted-public-keys = [ "cache.vpsadminos.org:wpIJlNZQIhS+0gFf1U3MC9sLZdLW3sh5qakOWGDoDrE=" ];
+  };
+
   system.stateVersion = "22.05";
 }

4c8522685bef92f6b44f6a3b797145f2984829e2
Jakub Skokan <jakub.skokan@havefun.cz>
2025-06-14 17:37:43 +0200

    int.builder: nix.sandboxPaths -> nix.settings.extra-sandbox-paths

--- cluster/org.vpsadminos/int.builder/config.nix
+++ cluster/org.vpsadminos/int.builder/config.nix
@@ -9,13 +9,12 @@
     git
   ];
 
-  nix.sandboxPaths = [
-    "/nix/var/cache/ccache"
-  ];
-
   nix.settings = {
     substituters = [ "https://cache.vpsadminos.org" ];
     trusted-public-keys = [ "cache.vpsadminos.org:wpIJlNZQIhS+0gFf1U3MC9sLZdLW3sh5qakOWGDoDrE=" ];
+    extra-sandbox-paths = [
+      "/nix/var/cache/ccache"
+    ];
   };
 
   system.stateVersion = "22.05";
